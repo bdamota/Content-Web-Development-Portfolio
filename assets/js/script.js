b40f4fe38268c2c7ad6a689f54af0d7c534cc811
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 01:24:26 -0700

    web work info
    Former-commit-id: 269072101b791418ccad74d28990e98f9cfb6839 [formerly 66e7ad8127bb411b5c5f43fb7a6af75c72833dea]
    Former-commit-id: 75ef0fbd743cf96ab1f602055f9f2187e4719158

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
   $('#modal1').on('show.bs.modal', function () { // on opening the modal
     // set the video to autostart
-    $("#modal1 iframe").attr("src", videoSrc+"&amp;autoplay=0");
+    $("#modal1 iframe").attr("src", videoSrc);
   });
   
   $("#modal1").on('hidden.bs.modal', function (e) { // on closing the modal
