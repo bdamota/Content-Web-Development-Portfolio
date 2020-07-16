0cdb8079de16a36ad843331aed5bbb32b4d95243
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 02:22:00 -0700

    function added to stop all videos and prevent autoplay
    Former-commit-id: 60c4bf67733650897e3ba3f4e7b466afe0bb95ea [formerly 267f781bef56f182fb86811a8fa6692357bc0099]
    Former-commit-id: 6511366169ddca4ffd73afe9b723d9de0b22ea5b

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,80 @@
-  var videoSrc = $("#modal1 video").attr("src");
+  var videoSrc1 = $("#modal1 video").attr("src");
   $("#modal1").on("hide.bs.modal", function(e) {
-    // a poor man's stop video
-    $("#video").attr("src", videoSrc);
+    // stop video
+    $("#video1").attr("src", videoSrc1);
   });
+
+  var videoSrc2 = $("#modal2 video").attr("src");
+  $("#modal2").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video2").attr("src", videoSrc2);
+  });
+
+  var videoSrc3 = $("#modal3 video").attr("src");
+  $("#modal3").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video3").attr("src", videoSrc3);
+  });
+
+  var videoSrc4 = $("#modal4 video").attr("src");
+  $("#modal4").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video4").attr("src", videoSrc4);
+  });
+
+
+  var videoSrc5 = $("#modal5 video").attr("src");
+  $("#modal5").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video5").attr("src", videoSrc5);
+  });
+
+  var videoSrc6 = $("#modal6 video").attr("src");
+  $("#modal6").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video6").attr("src", videoSrc6);
+  });
+
+  var videoSrc7 = $("#modal7 video").attr("src");
+  $("#modal7").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video7").attr("src", videoSrc7);
+  });
+
+  var videoSrc8 = $("#modal8 video").attr("src");
+  $("#modal8").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video8").attr("src", videoSrc8);
+  });
+
+  var videoSrc9 = $("#modal9 video").attr("src");
+  $("#modal9").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video9").attr("src", videoSrc9);
+  })
+
+  var videoSrc10 = $("#modal10 video").attr("src");
+  $("#modal10").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video10").attr("src", videoSrc10);
+  })
+
+  var videoSrc11 = $("#modal11 video").attr("src");
+  $("#modal11").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video11").attr("src", videoSrc11);
+  })
+
+
+  var videoSrc12 = $("#modal12 video").attr("src");
+  $("#modal12").on("hide.bs.modal", function(e) {
+    // stop video
+    $("#video12").attr("src", videoSrc12);
+  })
+
+
+
+
+  
+
+  
