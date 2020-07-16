6c7e8ccbc2071ab7edf249215f029cbeda69c5da
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 01:44:02 -0700

    video test 4
    Former-commit-id: 18738f07a1c8fcf90142112ce35903f2b553df84 [formerly 1455a79cbd3c180dcdfc0f67d5ed180b2d631683]
    Former-commit-id: 77e2e9dbc174f78826789d0e0d231b601cae5fd6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,5 @@
-      
   var videoSrc = $("#modal1 video").attr("src");
-
-  // $('#modal1').on('show.bs.modal', function () { // on opening the modal
-  //   // set the video to autostart
-  //   $("#modal1 video").attr("src", videoSrc);
-  // });
-  
-  $("#modal1").on('hidden.bs.modal', function (e) { // on closing the modal
-    // stop the video
-    $("#modal1 video").attr("src", null);
+  $("#modal1").on("hide.bs.modal", function(e) {
+    // a poor man's stop video
+    $("#video").attr("src", videoSrc);
   });
