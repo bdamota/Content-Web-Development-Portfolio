5e59e183fd450658eb41bc17fabd39543fc2de88
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 01:20:49 -0700

    video test 2
    Former-commit-id: ea0d570321ecf4ce5e410d6f9cfde95a34a75dfc [formerly b259ea88dcf41d55e44f8174a2be00983ef555b0]
    Former-commit-id: 4b40f9cd991ffc285941586640dd5ec3f14c81c0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,9 @@
-
-
-  // $(document).ready(function() {
-  //   $('#modal1').on('hidden.bs.modal', function() {
-  //     var $this = $(this).find('iframe');
-  //     tempSrc = $this.attr('src');
-  //         $this.attr('src', "");
-  //   });
-  
-
-  //   $(document).ready(function() {
-  //       $('#modal1').on('shown.bs.modal', function() {
-  //         var $this = $(this).find('iframe'),
-  //           tempSrc = $this.attr('src');
-  //         $this.attr('src', tempSrc);
-  //       })});
-  //   })
       
   var videoSrc = $("#modal1 iframe").attr("src");
 
   $('#modal1').on('show.bs.modal', function () { // on opening the modal
     // set the video to autostart
-    $("#modal1 iframe").attr("src", videoSrc);
+    $("#modal1 iframe").attr("src", videoSrc+"&amp;autoplay=0");
   });
   
   $("#modal1").on('hidden.bs.modal', function (e) { // on closing the modal
