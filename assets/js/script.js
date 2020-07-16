0c034092e9779bf4144ba37bc2d982aa8615f797
LYNN DAMOTA <bdamota0506@gmail.com>
2020-07-16 01:34:25 -0700

    video test 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
       
-  var videoSrc = $("#modal1 iframe").attr("src");
+  var videoSrc = $("#modal1 video").attr("src");
 
-  $('#modal1').on('show.bs.modal', function () { // on opening the modal
-    // set the video to autostart
-    $("#modal1 iframe").attr("src", videoSrc);
-  });
+  // $('#modal1').on('show.bs.modal', function () { // on opening the modal
+  //   // set the video to autostart
+  //   $("#modal1 video").attr("src", videoSrc);
+  // });
   
   $("#modal1").on('hidden.bs.modal', function (e) { // on closing the modal
     // stop the video
-    $("#modal1 iframe").attr("src", null);
+    $("#modal1 video").attr("src", null);
   });
